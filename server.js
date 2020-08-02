e23f5ed5146247b9f11f795499b5858c89c4bb53
Ishaan-11 <ishaansharma1998@gmail.com>
2020-08-02 13:45:09 +0530

    18.3

--- server.js
+++ server.js
@@ -2,6 +2,13 @@ const express = require('express');
 
 const app = express();
 
+// app.get("/", function(request,response){
+app.get("/", function(req,res) {
+    // console.log(request);
+    // response.send("hello");
+    res.send("<h1>Hello World!</h1>");
+});
+
 app.listen(3000, function() {
     console.log('Server started on port 3000');
 });
